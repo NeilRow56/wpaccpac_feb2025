aa8d3678c6ab0d90e780aa53b5e81ecb75f750db
NeilRow56 <neilrowland56@gmail.com>
2025-02-16 14:28:04 +0000

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hie937imukqznlzt.public.blob.vercel-storage.com'
+      },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'static.vecteezy.com' },
+      { protocol: 'https', hostname: 'avatar.vercel.sh' },
+      { protocol: 'https', hostname: 'utfs.io' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: 'images.pexels.com' }
+    ]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
---
+++ src/lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Prostore'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'A modern ecommerce store built with Next.js'
+//export const SERVER_URL =
+//  process.env.NEXT_PUBLIC_SERVER_URL || 'https://next-learn-dashboard.vercel.sh' FOR PRODUCTION ONLY

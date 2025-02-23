bd777fd424cbdcb8e5ac637edfeed2ee7b5bafd8
NeilRow56 <neilrowland56@gmail.com>
2025-02-23 06:33:58 +0000

    Updated eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,7 @@ const eslintConfig = [
       'react/no-unescaped-entities': 'off',
 
       '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/no-empty-object-type': 'error',
+
       '@typescript-eslint/no-explicit-any': 'off'
     }
   })
